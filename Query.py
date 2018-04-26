6d25039b53393c5a78c66c118d844c1ed4e63ee1
Travis Kern <kern.travis@gmail.com>
2018-04-26 18:56:20 -0400

    Abstract time logic and create a query result set
    Pulled the timer code out of QueryResult and moved it up to its class,
    TimedOperation.
    
    Created a new class, QueryResultSet, which is just a group of query
    results. A QueryResultSet also subclasses off TimedOperation, making
    it easier to track the total duration of batch operations.
    
    QueryResultSet also has a total_count() method, which will iterate
    through all of its query results for a total rowcount.

--- Query.py
+++ Query.py
@@ -1,5 +1,38 @@
 import datetime
 
+class TimedOperation(object):
+	def start_timer(self):
+		self.start_time = datetime.datetime.now()
+
+	def end_timer(self):
+		self.end_time = datetime.datetime.now()
+
+	def reset_times(self):
+		self.start_time = datetime.datetime.now()
+		self.end_time = datetime.datetime.now()
+
+	def format_duration(self):
+		ms = self.duration_ms()
+		secs = self.duration_seconds()
+		mins = self.duration_minutes()
+
+		if mins != 0:
+			return "%sm" % mins
+		elif secs != 0:
+			return "%sm" % secs
+		else:
+			return "%sms" % ms
+
+	def duration_ms(self):
+		diff = self.end_time - self.start_time
+		return int((diff.seconds * 1000) + (diff.microseconds / 1000))
+
+	def duration_seconds(self):
+		return int(self.duration_ms() / 1000)
+
+	def duration_minutes(self):
+		return int(self.duration_seconds() / 60)
+
 class Query(object):
 	def __init__(self, sql, params):
 		self.sql = sql
@@ -13,53 +46,50 @@ class NamedQuery(Query):
 		super(NamedQuery, self).__init__(sql, params)
 		self.name = name
 
-class QueryResult(object):
+class QueryResult(TimedOperation):
 	def __init__(self, query):
 		self.query = query
-		self.start_time = datetime.datetime.now()
-		self.end_time = datetime.datetime.now()
 
 		self.rowcount = 0
 		self.rows = []
 
-	def start_timer(self):
-		self.start_time = datetime.datetime.now()
-
-	def end_timer(self):
-		self.end_time = datetime.datetime.now()
-
-	def format_time(self):
-		ms = self.timed_ms()
-		secs = self.timed_seconds()
-		mins = self.timed_minutes()
-
-		if mins != 0:
-			return "%sm" % mins
-		elif secs != 0:
-			return "%sm" % secs
-		else:
-			return "%sms" % ms
-
-	def timed_ms(self):
-		diff = self.end_time - self.start_time
-		return int((diff.seconds * 1000) + (diff.microseconds / 1000))
-
-	def timed_seconds(self):
-		return int(self.timed_ms() / 1000)
-
-	def timed_minutes(self):
-		return int(self.timed_seconds() / 60)
-	
 	def clear_rows(self):
 		self.rows[:] = []
-	
-	def reset_times(self):
-		self.start_time = datetime.datetime.now()
-		self.end_time = datetime.datetime.now()
 	
 	def forEach(self, handler):
 		for r in self.rows:
 			handler(r)
+	
+	def add_count(self, count):
+		self.rowcount += count
+
+class QueryResultSet(TimedOperation):
+	def __init__(self, title):
+		self.title = title
+		self._results = []
+	
+	def add_result(self, result):
+		self._results.append(result)
+	
+	def add_results(self, results):
+		self._results.extend(results)
+	
+	def results(self):
+		return self._results
+	
+	def first(self):
+		return self._results[0]
+	
+	def last(self):
+		return self._results[len(self._results)-1]
+	
+	def total_count(self):
+		count = 0
+
+		for res in self._results:
+			count += res.rowcount
+
+		return count
 
 def create_query(sql, params):
 	return Query(sql, params)
